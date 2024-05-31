9b667595160879ddebd052cd17de4e4a46243262
unknown <santiago.vargas5@udea.edu.co>
2024-05-30 19:03:50 -0500

    Archivos

---
+++ main.h
@@ -0,0 +1,14 @@
+/**
+ * @file main.h
+ * @brief This is the header file for the main.c file
+ */
+ 
+// Avoid duplication in code
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+
+void mainasm();
+ 
+
+#endif
